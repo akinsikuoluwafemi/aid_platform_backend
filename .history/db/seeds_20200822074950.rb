32d2092b99aed52ec9b960c5520df170a5a5dcb4
Akinsiku Femi <akinsiku13@gmail.com>
2020-08-22 09:24:54 +0100

    Bring in active storage for images

---
+++ .history/db/seeds_20200822074950.rb
@@ -0,0 +1,21 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+
+user = User.create(
+    first_name: 'Prince',
+    last_name: 'Odumeje',
+    email: 'prince@yahoo.com',
+    user.image.attach(
+        io: File.open('app/assets/images/boys.jpeg'),
+                filename: 'boys.jpeg',
+                content_type: 'application/jpeg',
+    )
+)
+
+
